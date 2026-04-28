e39ac509bd814346ee5dc176e805d29af65c01d1
raxmatilla031605 <raxmatilla031605@gmail.com>
2026-04-28 14:36:58 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+let a = Number(prompt("son kiriting"));
+
+if (a % 2 == 0){
+    console.log("juft son");
+} else {
+    console.log("toq son");
+}
+
+// let son1 = (prompt("Birinchi sonni kiriting:"));
+// let son2 = (prompt("Ikkinchi sonni kiriting:"));
+
+// son1 = Number(son1);
+// son2 = Number(son2);
+
+// if (son1 > son2) {
+//     console.log("kattasi: " + son1);
+// } else if (son2 > son1) {
+//   console.log("kattasi: " + son2);
+// }
+
+// let tugilganYil = prompt("Tug‘ilgan yilingizni kiriting:");
+
+// let hozirgiYil = new Date().getFullYear();
+
+
+// let yosh = hozirgiYil - tugilganYil;
+
+
+// alert("Sizning yoshingiz: " + yosh);
+
+
+// let a = prompt("1-sonni kiriting:");
+// let b = prompt("2-sonni kiriting:");
+// let c = prompt("3-sonni kiriting:");
+
+// let max = a;
+
+// if (b > max) {
+//     max = b;
+// }
+// if (c > max) {
+//     max = c;
+// }
+
+// alert("Eng katta son: " + max);
+
+
+// let son1 = Number(prompt("Birinchi sonni kiriting:"));
+// let son2 = Number(prompt("Ikkinchi sonni kiriting:"));
+// let amal = prompt("Amalni kiriting (+, -, *, /):");
+
+// let natija;
+
+// if (amal === "+") {
+//     natija = son1 + son2;
+// } else if (amal === "-") {
+//     natija = son1 - son2;
+// } else if (amal === "*") {
+//     natija = son1 * son2;
+// } else if (amal === "/") {
+//     if (son2 !== 0) {
+//         natija = son1 / son2;
+//     } else {
+//         natija = "0 ga bo‘lish mumkin emas!";
+//     }
+// } else {
+//     natija = "Noto‘g‘ri amal kiritildi!";
+// }
+
+// alert("Natija: " + natija);
+
+
+// let n = Number(prompt( "n ni kiriting")); 
+// let yigindi = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     yigindi += i;
+// }
+
+// console.log("yigindi:", yigindi); 
+
+
+// let n = Number(prompt("Son kiriting:"));
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(n + " x " + i + " = " + (n * i));
+// }
